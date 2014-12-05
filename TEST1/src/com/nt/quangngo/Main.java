ed674631397d5326fc94291a0fc42eb58776273f
quangngo <quang.ngo@niteco.se>
2014-12-05 13:23:17 +0700

    Quang cmt

---
+++ TEST1/src/com/nt/quangngo/Main.java
@@ -0,0 +1,9 @@
+package com.nt.quangngo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("TEST2. Made by Quang Ngo Doan");
+    }
+}
